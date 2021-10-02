68a007df13e793baa538918af6bae2528132b111
RobertoMendezM <91458910+RobertoMendezM@users.noreply.github.com>
2021-10-02 08:12:59 -0500

    Delete 2 Armonica.R

--- Sesión2_MedidasTendenciaCentral/2 Armonica.R
+++
@@ -1,4 +0,0 @@
-#Ejercicio 2:
-#En un autodromo estan 20 autos de carreras compitiendo 
-#determina la media geometrica si sus velocidades son:
-# 321, 367, 310, 378, 370, 354, 367, 359, 377, 375, 282, 365, 371, 310, 315, 355, 345, 365, 348, 369
