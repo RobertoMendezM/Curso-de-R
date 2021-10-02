a2fa1494293215f7b52b1a358af2cd7565f1da4a
RobertoMendezM <91458910+RobertoMendezM@users.noreply.github.com>
2021-10-02 08:13:42 -0500

    Add files via upload

---
+++ Sesión2_MedidasTendenciaCentral/Ejercicio_MediaGeometrica.R
@@ -0,0 +1,5 @@
+#Ejercicio 2:
+
+#En un autodromo están 20 autos de carreras compitiendo 
+#determina la media geomátrica si sus velocidades son:
+# 321, 367, 310, 378, 370, 354, 367, 359, 377, 375, 282, 365, 371, 310, 315, 355, 345, 365, 348, 369
