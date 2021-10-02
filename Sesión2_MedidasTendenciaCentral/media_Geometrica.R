6da7ae04927119e8330f7de2e0f8b1b83167eb18
RobertoMendezM <91458910+RobertoMendezM@users.noreply.github.com>
2021-10-02 03:47:49 -0500

    Add files via upload

---
+++ Sesión2_MedidasTendenciaCentral/media_Geometrica.R
@@ -0,0 +1,21 @@
+# Programa que calcula la Media Geométrica
+
+# Curso: Estadística con R 
+
+# Fecha: 21/Septiembre/2021
+
+# Autor: Cesar Yael Macias y Roberto Méndez
+
+datos <- c(6, 2, 3, 4, 5, 1, 7, 8, 9, 10)
+
+n <- length(datos)
+producto <- 1
+
+# Calcula el producto de los datos
+for(i in datos){
+  producto <- producto*i
+}
+
+media_Geometrica <- producto**(1/n)
+
+print(paste("La media Geometrica es:", media_Geometrica))
