0f461451fec7a9c5a87ba27b840e2478b8563bd1
RobertoMendezM <91458910+RobertoMendezM@users.noreply.github.com>
2021-10-02 02:22:09 -0500

    Update and rename Media.r to mediaAritmetica.R

---
+++ Sesión2_MedidasTendenciaCentral/mediaAritmetica.R
@@ -0,0 +1,14 @@
+# Programa que calcula la Media Aritmética
+# Curso en R en una semana
+# 
+# Autoras: Alexia Pamela Sanchez de Tagle Herrera y Ramos Mundo Jimena 
+# Fecha: 17 de Septiembre de 2021
+
+datos <- c(55,36,28)
+
+suma <- sum(datos)
+n <-length(datos)
+media <- suma/n
+
+print(paste("la media es", media))
+
